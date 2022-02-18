d0d46bf247769863ddf35af86f54060e4cf37db9
Jvnyor <josias100junior@gmail.com>
2022-02-18 15:43:09 -0300

    first commit

---
+++ src/com/Jvnyor/passwords/Main.java
@@ -0,0 +1,17 @@
+package com.Jvnyor.passwords;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Generating password...");
+			
+			String letters = "";
+			String numbers = "";
+			
+			System.out.println("");
+		} finally {
+			
+		}
+	}
+}
